b4c2e0aae6dedcfd31a45e49f2b7128fa48e3dae
Hassan Khalil <khalil@Hassans-MacBook-Pro.local>
2024-10-14 07:17:57 +0100

    Add backend files for text analyzer

---
+++ t-a backend/src/main/java/text_analyser/TextAnalyserApplication.java
@@ -0,0 +1,13 @@
+package text_analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextAnalyserApplication.class, args);
+	}
+
+}
---
+++ t-a backend/src/test/java/text_analyser/TextAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package text_analyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
